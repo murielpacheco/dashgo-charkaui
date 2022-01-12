56de91fdc1c3f3f5f88d721edcfba29b5853b061
Muriel Damazio <murieldps@gmail.com>
2022-01-12 08:40:43 -0300

    Feat: Changing pages done

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -1,81 +1,46 @@
-import { Stack, Button, Box, Text } from "@chakra-ui/react";
-import { PaginationItem } from "./PaginationItem";
+import { Button } from "@chakra-ui/button";
 
-interface PaginationProps {
-  totalCountOfRegisters: number;
-  registersPerPage?: number;
-  currentPage?: number;
+interface PaginationItemProps {
+  number: number;
+  isCurrent?: boolean;
   onPageChange: (page: number) => void;
 }
 
-const siblingsCount = 1;
-
-function generatePagesArray(from: number, to: number) {
-  return [...new Array(to - from)]
-    .map((_, index) => {
-      return from + index + 1;
-    })
-    .filter(page => page > 0)
-}
-
-export function Pagination({
-  totalCountOfRegisters,
-  registersPerPage = 10,
-  currentPage = 1,
+export function PaginationItem({ 
+  isCurrent = false, 
   onPageChange,
-}: PaginationProps) {
-  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
-
-  const previousPages = currentPage > 1
-    ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
-    : []
-
-  const nextPages = currentPage < lastPage
-    ? generatePagesArray(currentPage, Math.min(currentPage + siblingsCount, lastPage))
-    : []
+  number
+}: PaginationItemProps) {
+  if (isCurrent) {
+    return (
+      <Button
+        size="sm"
+        fontSize="xs"
+        width="4"
+        colorScheme="pink"
+        disabled
+        _disabled={{
+          bg: 'pink.500',
+          cursor: 'default',
+        }}
+      >
+        {number}
+      </Button>
+    )
+  }
 
   return (
-    <Stack
-      direction={["column", "row"]}
-      spacing="6"
-      mt="8"
-      justify="space-between"
-      align="center"
+    <Button
+      size="sm"
+      fontSize="xs"
+      width="4"
+      bg="gray.700"
+      _hover={{
+        bg: 'gray.500'
+      }}
+      onClick={() => onPageChange(number)}
     >
-      <Box>
-        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
-      </Box>
-      <Stack direction="row" spacing="2">
-
-        {currentPage > (1 + siblingsCount) && (
-          <>
-            <PaginationItem onPageChange={onPageChange} number={1} />
-            { currentPage > (2 + siblingsCount) && (
-              <Text color="gray.300" width="8" textAlign="center">...</Text>
-            )}
-          </>
-        )}
-
-        {previousPages.length > 0 && previousPages.map(page => {
-          return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
-        })}
-
-        <PaginationItem onPageChange={onPageChange} number={currentPage} isCurrent />
-
-        {nextPages.length > 0 && nextPages.map(page => {
-          return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
-        })}
-
-        {(currentPage + siblingsCount) < lastPage && (
-          <>
-            { (currentPage + 1 + siblingsCount) < lastPage && (
-              <Text color="gray.300" width="8" textAlign="center">...</Text>
-            )}
-            <PaginationItem onPageChange={onPageChange} number={lastPage} />
-          </>
-        )}
-        
-      </Stack>
-    </Stack>
-  );
+      {number}
+    </Button>
+  )
 }
